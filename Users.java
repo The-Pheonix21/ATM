7a6652b0dbe4d6a763f8ae3868caeadb6762c6b9
Chris <c5keith90@gmail.com>
2013-10-10 09:15:00 -0600

    yep

---
+++ Users.java
@@ -0,0 +1,8 @@
+public class Users {
+  Account banker 1 = new Account("Bob", 1234, 100.0);
+  Account banker 2 = new Account("Alice", 3333, 250.0);
+  Account banker 3 = new Accounts("JT", 0001, 999999999.0);	
+  public int checkBallence() {
+
+  }
+}
