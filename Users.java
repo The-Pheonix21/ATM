56af49a1a9df43937848a4d8a40a1c28bb181edb
Chris <c5keith90@gmail.com>
2013-10-20 08:49:23 -0600

    ok

--- Users.java
+++ Users.java
@@ -1,9 +1,57 @@
 public class Users {
-  Account banker 1 = new Account("Bob", 1234, 100.0);
-  Account banker 2 = new Account("Alice", 3333, 250.0);
-  Account banker 3 = new Account("JT", 0001, 999999999.0);	
+ private String[] cust_name = {"Bob" ,"Allice" ,"JT"};
+ private int [] cust_Pin = {1234, 3333, 0001};
+ private double [] cust_Balance = {100.0, 250.0, 999999999.0};
+ private int index_cust;
   public Users()
-	public int checkBallence() {
-
-  	}
+	public boolean findCust(String name, int pin) {
+		index_cust=-1;
+		for (int i=0; i < cust_namelength;i++) {
+		if (name.equals(cust_name[i]) && cust_Pin[i]==pin) {
+			index_cust = i;
+			System.out.println("Welcome Valued customer");
+			return true;
+		}
+			
+		}
+		if (index_cust < 0) {
+			System.out.println();
+			System.out.println("I'm sorry we can't locate this customer.");
+			System.out.println("Please try again.");
+			System.out.println();
+			return false;
+		}
+		return true;
+	}
+	public void changeName(String name) {
+		cust_name [index_cust]=name;
+		System.out.println();
+		System.out.println(" THe name on your account has been changed to: " +cust_name[index_cust]+"\n\n");
+	} 
+	public void changePin(int pin){
+		cust_Pin[index_cust]=pin;
+		System.out.println();
+		System.out.println(" The pin number on your account has been changed to: "+ cust_Pin[index_cust]+"\n\n");
+	} 
+	public void widthdrawMoney(double w){
+		if (cust_Balence[index_cust]==0) {
+			System.out.println("\tYour current balance is zero.");
+			System.out.println("\tYou cannot widthdraw!!!");
+			System.out.println("\tYou need to deposit money first");
+		}else if (w > cust_Balance) {
+			System.out.println("\tThe amount you want to widthdraw is greater than the balence you currently have");
+			System.out.println("\tPlease check the amount you entered");
+		}else{
+			setbalance(getBalance()-w);
+			System.out.println("\n\tYou withdrew the amount of " + w);
+			System.out.println("\tYou're current balance is " + getBalance());
+		}
+		
+	}
+public void setBalance(double b){
+	cust_Balance[index_cust] = b; 
+}
+public double getBalance(){
+	return cust_Balance[index_cust];
+}
 }
